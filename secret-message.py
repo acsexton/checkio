6c8be930064d6b9562d1678b44e21adb4d95bbd2
acsexton <andrew@acsexton.com>
2017-08-03 18:10:01 -0400

    Message complete

---
+++ secret-message.py
@@ -0,0 +1,18 @@
+def find_message(text):
+    """Find a secret message"""
+    caps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    decode = []
+    text = list(text)
+    for x in text:
+        if x in caps:
+            decode.append(x)
+        else:
+            pass
+    print(''.join(decode))
+    return ''.join(decode)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert find_message("How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
+    assert find_message("hello world!") == "", "Nothing"
+    assert find_message("HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
