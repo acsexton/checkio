1ed732051441b89df8f43f8433855b16f964eb43
acsexton <andrew@acsexton.com>
2018-04-29 18:54:00 -0400

    started with hackerrank

---
+++ hackerrank/problem04.py
@@ -0,0 +1,17 @@
+# Task
+# Read an integer . For all non-negative integers , print . See the sample for details.
+#
+# Input Format
+#
+# The first and only line contains the integer, .
+#
+# Constraints
+#
+# Output Format
+#
+# Print lines, one corresponding to each .
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0, n):
+        print(i**2)
